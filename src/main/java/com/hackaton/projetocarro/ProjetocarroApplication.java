37570e06fb8826f5572cfddbb177f59554aff2b2
unknown <guilhermeasmorais@gmail.com>
2024-11-22 21:53:02 -0300

    first commit

---
+++ src/main/java/com/hackaton/projetocarro/ProjetocarroApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.projetocarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton/projetocarro/ProjetocarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.projetocarro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
